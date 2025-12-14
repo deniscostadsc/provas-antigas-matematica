41b3c7bdfec16484e1de1703c1d1e7e4c1263d80
Denis Costa <deniscostadsc@gmail.com>
2025-12-14 15:38:23 -0300

    Renomeia provas do colégio militar do RJ

--- scripts/renomear.sh
+++ scripts/renomear.sh
@@ -3,12 +3,13 @@
 # set -vx
 set -euo pipefail
 
-FOLDER="provas/olimpiadas-de-matematica/obmep/nivel-2/"
+FOLDER="provas/concursos-militares/ensino-fundamental/colegio-militar/rio-de-janeiro/"
+
 cd "$FOLDER"
 
 function extract_year {
     local filename=${1}
-    local year_pattern='([12][09][0-9][0-9])'
+    local year_pattern='cmrj-([12][09][0-9][0-9])'
 
     if [[ "${filename}" =~ ${year_pattern} ]]; then
         echo "${BASH_REMATCH[1]}"
@@ -32,6 +33,9 @@ function extract_phase {
 }
 
 function extract_level {
+    echo "ensino-fundamental"
+    return 0
+
     local filename=${1}
     local level_pattern='(n)(2)'
 
@@ -46,45 +50,45 @@ function extract_level {
 
 function extract_file_type {
     local filename=${1}
-    local file_type_pattern='^p'
+    local file_type_pattern='gabarito'
     local year=$(extract_year "${filename}")
 
     if [[ "${filename}" =~ ${file_type_pattern} ]]; then
         echo "prova"
         return 0
     else
-        echo "resolucao"
+        echo "gabarito"
         return 0
     fi
 }
 
-declare -A phase_cache
-declare -A count_per_year_cache
-for file in *.pdf; do
-    if [[ -f "${file}" ]]; then
-        year=$(extract_year "${file}")
-        phase=$(extract_phase "${file}")
-        count_per_year_cache["${year}"]=$(( ${count_per_year_cache[${year}]:-0} + 1 ))
-        if [[ -n "${year}" && -n "${phase}" ]]; then
-            if [[ ! -v phase_cache["${year}"] ]]; then
-                phase_cache["${year}"]="${phase}"
-            fi
-        fi
-    fi
-done
+# declare -A phase_cache
+# declare -A count_per_year_cache
+# for file in *.pdf; do
+#     if [[ -f "${file}" ]]; then
+#         year=$(extract_year "${file}")
+#         phase=$(extract_phase "${file}")
+#         count_per_year_cache["${year}"]=$(( ${count_per_year_cache[${year}]:-0} + 1 ))
+#         if [[ -n "${year}" && -n "${phase}" ]]; then
+#             if [[ ! -v phase_cache["${year}"] ]]; then
+#                 phase_cache["${year}"]="${phase}"
+#             fi
+#         fi
+#     fi
+# done
 
 
 for file in *.pdf; do
     if [[ -f "${file}" ]]; then
         year=$(extract_year "${file}")
         level=$(extract_level "${file}")
-        phase="${phase_cache[${year}]}"
+        # phase="${phase_cache[${year}]}"
         file_type=$(extract_file_type "${file}")
 
-        mv "${file}" "${year}-${level}-${phase}-obm-${file_type}.pdf"
+        mv "${file}" "${year}-${level}-cm-rj-${file_type}.pdf"
         echo
         echo "${file}"
-        echo "${year}-${level}-${phase}-obm-${file_type}.pdf"
+        echo "${year}-${level}-cm-rj-${file_type}.pdf"
         echo 
     fi
 done
